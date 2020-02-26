d5056df0dd70239ddf6f131091f264a3262741c0
Bai Xuepeng <ruixijiejie@gmail.com>
2020-02-26 11:58:43 +0800

    系统添加所有菜单,角色管理页面接口调试,通用CRUD组件进一步封装

--- manage-web/src/api/news/news.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function getNewsList(data) {
-  return request({
-    url: '/news/getNews',
-    method: 'post',
-    data
-  })
-}
--- manage-web/src/views/system/role/index.vue
+++ manage-web/src/views/system/role/index.vue
@@ -1,8 +1,29 @@
 <template>
   <div class="app-container">
     <!--工具栏-->
-    <crudOperation />
+    <crudOperation :permission="permission" />
     <!-- 表单渲染 -->
+    <el-dialog
+      append-to-body
+      :close-on-click-modal="false"
+      :before-close="crud.cancelCU"
+      :visible.sync="crud.status.cu > 0"
+      :title="crud.status.title"
+      width="450px"
+    >
+      <el-form ref="form" :inline="true" :model="form" :rules="rules" size="small" label-width="110px">
+        <el-form-item label="角色名称" prop="role">
+          <el-input v-model="form.role" style="width: 185px;" />
+        </el-form-item>
+        <el-form-item label="描述信息" prop="descr">
+          <el-input v-model="form.descr" style="width: 185px;" rows="2" type="textarea" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="text" @click="crud.cancelCU">取消</el-button>
+        <el-button :loading="crud.status.cu === 2" type="primary" @click="crud.submitCU">确认</el-button>
+      </div>
+    </el-dialog>
     <el-row :gutter="15" style="margin-top: 12px;">
       <!--角色管理-->
       <el-col :xs="24" :sm="24" :md="16" :lg="16" :xl="17" style="margin-bottom: 10px">
@@ -66,16 +87,30 @@ import pagination from '@crud/Pagination'
 
 // crud交由presenter持有
 const defaultCrud = CRUD({ title: '角色', url: '/back/sysRole/lists', crudMethod: { ...crudRoles }})
-const defaultForm = { id: null, name: null, remark: null }
+const defaultForm = { role: null, descr: null }
 export default {
   name: 'Role',
   components: { pagination, crudOperation, udOperation },
   mixins: [presenter(defaultCrud), header(), form(defaultForm), crud()],
   data() {
     return {
+      permission: {
+        add: true,
+        edit: false,
+        del: true,
+        refresh: true
+      },
+      rules: {
+        role: [
+          { required: true, message: '请输入名称', trigger: 'blur' }
+        ],
+        descr: [
+          { required: true, message: '请输入描述信息', trigger: 'blur' }
+        ]
+      },
       defaultProps: { children: 'children', label: 'label' },
-      currentId: 0, menuLoading: false, showButton: false,
-      menus: [], menuIds: []
+      menuLoading: false,
+      showButton: false
     }
   },
   created() {
@@ -94,10 +129,10 @@ export default {
 
     },
     [CRUD.HOOK.afterAddError](crud) {
-      this.afterErrorMethod(crud)
+
     },
     [CRUD.HOOK.afterEditError](crud) {
-      this.afterErrorMethod(crud)
+
     },
     // 触发单选
     handleCurrentChange(val) {
